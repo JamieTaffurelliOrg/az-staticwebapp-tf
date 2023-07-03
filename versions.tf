c117c93c3945f4f2b968b63ad6d8121e3e70c59c
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-07-03 14:55:46 +0100

    corrected providers

--- versions.tf
+++ versions.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      configuration_aliases = [azurerm.logs, azurerm.dns, azurerm.hub]
-      source                = "hashicorp/azurerm"
-      version               = "~> 3.20"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.20"
     }
   }
   required_version = "~> 1.5.0"
